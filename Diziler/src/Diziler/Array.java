07f3ae4d327dc6eba98ed0464ea663fd4ee84e50
sdsag <sdsag@LAPTOP-HPAHPA94>
2022-10-27 10:16:06 +0300

    First

---
+++ Diziler/src/Diziler/Array.java
@@ -0,0 +1,16 @@
+package Diziler;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int[]array = new int[10];
+   System.out.printf("%s%8%n","Index","Value");
+   for (int counter=0;counter<array.length;counter++) {
+	   System.out.printf("%5d%8d%n",counter,array[counter]);
+	   		
+   }
+	}
+
+}
+
---
+++ Diziler/src/Diziler/Arrays.java
@@ -0,0 +1,16 @@
+package Diziler;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]array = {32,27,64,18,95,14,90,70,60,37};
+		
+		
+		   System.out.printf("%s%8s%n","Index","Value");
+		   
+		   for (int counter=0;counter<array.length;counter++) {
+			   System.out.printf("%5d%8d%n",counter,array[counter]);}
+	}
+
+}
